5c5d0c7fe7097802f7dcde62e1ac4dc61c63b7db
Muhammad Hasan <contacttohasan@gmail.com>
2024-12-06 17:41:05 +0100

    first commit

---
+++ quitsmoking/quitsmokingApp.swift
@@ -0,0 +1,17 @@
+//
+//  quitsmokingApp.swift
+//  quitsmoking
+//
+//  Created by Muhammad Hasan on 06/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct quitsmokingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
